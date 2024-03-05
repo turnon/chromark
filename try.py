622222eaa40d9e16cd0e998cfe746d47786dd4f2
ken <block24block@gmail.com>
2024-03-05 14:57:24 +0800

    regexp closure

--- try.py
+++ try.py
@@ -1,41 +1,43 @@
 import re
 
-folder_start_pattern = r"<DT><H3"
-folder_end_pattern = r"</DL>"
-entry_pattern = r"<DT><A"
 
-folder_name_pattern = r"<H3.*>(.*?)</H3>"
+def regexp_extract_first(regexp):
+    def m(string):
+        match = re.search(regexp, string)
+        return match.group(1) if match else None
+
+    return m
 
 
 class Entry:
-    href_pattern = r'HREF="(.*?)"'
-    date_pattern = r'ADD_DATE="(.*?)"'
-    title_pattern = r"<A.*>(.*?)</A>"
+    xhref = regexp_extract_first(r'HREF="(.*?)"')
+    xdate = regexp_extract_first(r'ADD_DATE="(.*?)"')
+    xtitle = regexp_extract_first(r"<A.*>(.*?)</A>")
 
     def __init__(self, folders, line):
         self.folders = [fo for fo in folders]
-        self.url = regexp_extract(Entry.href_pattern, line)
-        self.date = regexp_extract(Entry.date_pattern, line)
-        self.title = regexp_extract(Entry.title_pattern, line)
+        self.url = self.__class__.xhref(line)
+        self.date = self.__class__.xdate(line)
+        self.title = self.__class__.xtitle(line)
 
     def __repr__(self):
         folder_str = "/".join(self.folders)
         return f"{folder_str}/{self.title}"
 
 
-def regexp_extract(regexp, string):
-    match = re.search(regexp, string)
-    return match.group(1) if match else None
-
-
 def iterator(bookmark_path):
+    folder_start_pattern = r"<DT><H3"
+    folder_end_pattern = r"</DL>"
+    entry_pattern = r"<DT><A"
+    extract_folder = regexp_extract_first(r"<H3.*>(.*?)</H3>")
+
     with open(bookmark_path) as f:
         stack = []
         for line in f:
             if re.search(entry_pattern, line):
                 yield Entry(stack, line)
             elif re.search(folder_start_pattern, line):
-                folder_name = regexp_extract(folder_name_pattern, line)
+                folder_name = extract_folder(line)
                 stack.append(folder_name)
             elif re.search(folder_end_pattern, line) and len(stack) > 0:
                 stack.pop()
